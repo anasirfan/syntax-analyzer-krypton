e5c49a245afe7d2f1acb1f97d93eed84eb777ea9
fayyazAli24 <113125426+fayyazAli24@users.noreply.github.com>
2023-08-29 11:34:03 +1000

    commit added

--- src/lexemes/WordBreaker.js
+++ src/lexemes/WordBreaker.js
@@ -46,21 +46,28 @@ class WordBreaker {
         const lexemes = [];
         let currentLexeme = '';
         let inString = false;
-
+    
         for (let i = 0; i < code.length; i++) {
             const char = code[i];
-
+    
             if (char === '"' && (i === 0 || code[i - 1] !== '\\')) {
                 inString = !inString;
             }
-
+    
             if (inString) {
                 currentLexeme += char;
                 continue;
             }
-
+            if (char === '/' && code[i + 1] === '/') {
+                // Handle "//" comment
+                while (i < code.length && code[i] !== '\n') {
+                    i++; // Move the index to the end of the comment
+                }
+                // No need to add anything to lexemes for comments
+            }
+    
             if (this.operatorMappings[char]) {
-               let combinedOperator = char;
+                let combinedOperator = char;
                 for (let j = i + 1; j < code.length; j++) {
                     const nextChar = code[j];
                     const combinedTest = combinedOperator + nextChar;
@@ -80,7 +87,7 @@ class WordBreaker {
     
                 lexemes.push(combinedOperator);
             } else if (this.punctuatorMappings[char] || this.wordBreakers.includes(char)) {
-               if (currentLexeme !== '') {
+                if (currentLexeme !== '') {
                     lexemes.push(currentLexeme);
                     currentLexeme = '';
                 }
@@ -92,7 +99,7 @@ class WordBreaker {
             } else {
                 // Handle numeric literals (both integer and floating-point)
                 if (/[0-9]/.test(char)) {
-                     currentLexeme += char;
+                    currentLexeme += char;
     
                     let isFloatingPoint = false;
                     for (let j = i + 1; j < code.length; j++) {
@@ -128,13 +135,108 @@ class WordBreaker {
                 }
             }
         }
-
+    
         if (currentLexeme !== '') {
             lexemes.push(currentLexeme);
         }
-
+    
         return lexemes;
     }
+    
+
+    // splitCode(code) {
+    //     const lexemes = [];
+    //     let currentLexeme = '';
+    //     let inString = false;
+
+    //     for (let i = 0; i < code.length; i++) {
+    //         const char = code[i];
+
+    //         if (char === '"' && (i === 0 || code[i - 1] !== '\\')) {
+    //             inString = !inString;
+    //         }
+
+    //         if (inString) {
+    //             currentLexeme += char;
+    //             continue;
+    //         }
+
+    //         if (this.operatorMappings[char]) {
+    //            let combinedOperator = char;
+    //             for (let j = i + 1; j < code.length; j++) {
+    //                 const nextChar = code[j];
+    //                 const combinedTest = combinedOperator + nextChar;
+    
+    //                 if (this.operatorMappings[combinedTest]) {
+    //                     combinedOperator = combinedTest;
+    //                     i = j; // Move the index to the last character of the combined operator
+    //                 } else {
+    //                     break;
+    //                 }
+    //             }
+    
+    //             if (currentLexeme !== '') {
+    //                 lexemes.push(currentLexeme);
+    //                 currentLexeme = '';
+    //             }
+    
+    //             lexemes.push(combinedOperator);
+    //         } else if (this.punctuatorMappings[char] || this.wordBreakers.includes(char)) {
+    //            if (currentLexeme !== '') {
+    //                 lexemes.push(currentLexeme);
+    //                 currentLexeme = '';
+    //             }
+    //             if (char === '\n') {
+    //                 lexemes.push('\n');
+    //             } else if (this.punctuatorMappings[char]) {
+    //                 lexemes.push(char);
+    //             }
+    //         } else {
+    //             // Handle numeric literals (both integer and floating-point)
+    //             if (/[0-9]/.test(char)) {
+    //                  currentLexeme += char;
+    
+    //                 let isFloatingPoint = false;
+    //                 for (let j = i + 1; j < code.length; j++) {
+    //                     const nextChar = code[j];
+    //                     if (nextChar === '.' && !isFloatingPoint) {
+    //                         isFloatingPoint = true;
+    //                         currentLexeme += nextChar;
+    //                     } else if (/[0-9]/.test(nextChar)) {
+    //                         currentLexeme += nextChar;
+    //                     } else {
+    //                         break;
+    //                     }
+    //                     i = j; // Move the index to the last character of the numeric literal
+    //                 }
+    
+    //                 lexemes.push(currentLexeme);
+    //                 currentLexeme = '';
+    //             } else if (this.isVariableStart(char)) {
+    //                 currentLexeme += char;
+    //                 for (let j = i + 1; j < code.length; j++) {
+    //                     const nextChar = code[j];
+    //                     if (this.isVariablePart(nextChar)) {
+    //                         currentLexeme += nextChar;
+    //                     } else {
+    //                         break;
+    //                     }
+    //                     i = j; // Move the index to the last character of the variable
+    //                 }
+    //                 lexemes.push(currentLexeme);
+    //                 currentLexeme = '';
+    //             } else {
+    //                 currentLexeme += char;
+    //             }
+    //         }
+    //     }
+
+    //     if (currentLexeme !== '') {
+    //         lexemes.push(currentLexeme);
+    //     }
+
+    //     return lexemes;
+    // }
 
     // Define the logic to check if the character is a valid starting character for a variable
     isVariableStart(char) {
